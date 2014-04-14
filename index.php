882d2fba5287c5b72173c91539c80f5640f6ff5d
Philihp Busby <philihp@gmail.com>
2014-04-14 07:57:38 +0000

    moved visibility to new tab. created rename tool.

--- index.php
+++ index.php
@@ -118,66 +118,75 @@ if(isset($user_id)) {
 	</head>
 
 <body>
+	<a href="#content" class="sr-only">Skip to main content</a>
 	<div class="container">
 		<div class="page-header">
+
+
 			<h2>Shipfam.com #Boatsnap</h2>
 			<p>
-			You are 
 			<?php if($user): ?>
-			<i><?=$user_profile['name']?></i> on the Facebook
-			<?php else: ?>
-			<i>not connected to the Facebook</i>
+			You are <i><?=$user_profile['name']?></i> on the Facebook
 			<?php endif ?>
 
 			<?php if(isset($snapchat)): ?>
 			and <i><?=$snapchat->username?></i> on Snapchat <a class="btn btn-default btn-xs" href="logout.php">Logout</a>
 			<?php endif ?>
 			</p>
+
+
+		<?php if(isset($snapchat)): ?>
+		<ul class="nav nav-pills" id="tabs">
+			<li class="active"><a href="#add" data-toggle="pill">Add Users</a></li>
+			<li><a href="#rename" data-toggle="pill">Rename Users</a></li>
+			<li><a href="#settings" data-toggle="pill">Settings</a></li>
+		</ul>
+		<?php endif ?>
 		</div>
+	</div>
+
+	<div class="container tab-content" id="content">
 		<?php
 		if(isset($err)) {
 			?>
-			<div class="alert alert-danger"><?=$err?></div>
+		<div class="alert alert-danger"><?=$err?></div>
 			<?php
 		}
 		?>
-	</div>
-
-	<div class="container">
 
 		<?php if (!$user): ?>
-			<p><a href="<?=$loginUrl?>"><img src="img/connect-fb.png" /></a></p>
+		<p><a href="<?=$loginUrl?>"><img src="img/connect-fb.png" /></a></p>
 		<?php elseif( !isset($snapchat)): ?>
-<form class="form-horizontal" action="index.php" method="POST" role="form">
-	<div class="form-group">
-		<label class="col-sm-4 control-label" for="snapchat_username">Snapchat Username</label>
-		<div class="col-sm-4">
-			<input type="text" class="form-control" name="snapchat_username" id="snapchat_username" placeholder="Username" value="<?=(isset($stored_username))?$stored_username:''?>">
-		</div>
-	</div>
-	<div class="form-group">
-		<label class="col-sm-4 control-label" for="snapchat_password">Snapchat Password</label>
-		<div class="col-sm-4">
-			<input type="password" class="form-control" name="snapchat_password" id="snapchat_password" placeholder="Password" />
-		</div>
-	</div>
-	<div class="form-group">
-		<div class="col-sm-offset-4 col-sm-10">
-			<button type="submit" class="btn btn-primary">Login to Snapchat</button>
-			<a href="nologin.php" class="btn btn-default">Skip</a>
-		</div>
-	</div>
-	<div>
-		<i>Why do you need my password?</i><br />
-		Snapchat doesn't expose a public API, so this webpage has to pretend to be the actual snapchat app and login to
-		their servers.
-Your password isn't stored. The <a href="https://github.com/shipfam/boatsnap">source code</a> for this is open, if you don't believe me. If you're super-paranoid, you can <a href="nologin.php">skip</a>
-the login, but you'll have to type in everyone's usernames again manually, which sucks, but whatever buddy, you're the masochist, not me.<br />
-<br />
-		<i>Why does it say my password is wrong?</i><br />
-		We occasionally get thrown into Snapchat-Jail for having too many connections to Snapchat. It sucks, but hopefully they'll whitelist us eventually.
-	</div>
-</form>
+		<form class="form-horizontal" action="<?=$_SERVER['PHP_SELF']?>" method="POST" role="form">
+			<div class="form-group">
+				<label class="col-sm-4 control-label" for="snapchat_username">Snapchat Username</label>
+				<div class="col-sm-4">
+					<input type="text" class="form-control" name="snapchat_username" id="snapchat_username" placeholder="Username" value="<?=(isset($stored_username))?$stored_username:''?>">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-4 control-label" for="snapchat_password">Snapchat Password</label>
+				<div class="col-sm-4">
+					<input type="password" class="form-control" name="snapchat_password" id="snapchat_password" placeholder="Password" />
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-4 col-sm-10">
+					<button type="submit" class="btn btn-primary">Login to Snapchat</button>
+					<a href="nologin.php" class="btn btn-default">Skip</a>
+				</div>
+			</div>
+			<div>
+				<i>Why do you need my password?</i><br />
+				Snapchat doesn't expose a public API, so this webpage has to pretend to be the actual snapchat app and login to
+				their servers.
+		Your password isn't stored. The <a href="https://github.com/shipfam/boatsnap">source code</a> for this is open, if you don't believe me. If you're super-paranoid, you can <a href="nologin.php">skip</a>
+		the login, but you'll have to type in everyone's usernames again manually, which sucks, but whatever buddy, you're the masochist, not me.<br />
+		<br />
+				<i>Why does it say my password is wrong?</i><br />
+				We occasionally get thrown into Snapchat-Jail for having too many connections to Snapchat. It sucks, but hopefully they'll whitelist us eventually.
+			</div>
+		</form>
 
 		<?php else: ?>
 
@@ -205,19 +214,10 @@ catch(FacebookApiException $e) {
 ?>
 
 
-<div class="table-responsive">
-	<table class="table-hover" id="users">
-		<tbody>
-			<tr>
-				<td><a href="//facebook.com/<?=$user_profile['id']?>" target="_blank"><img src="//graph.facebook.com/<?=$user_profile['id']?>/picture" /></a></td>
-				<td><?=$user_profile['name']?></td>
-				<td><?=$_SESSION['snapchat_username']?></td>
-				<td class="visibility-box">
-					<div class="btn-group">
-						<input type="checkbox" class="hider" <?=(!$hidden)?"checked":""?>>
-					</div>
-				</td>
-			</tr>
+			<div class="tab-pane fade active in" id="add">
+				<div class="table-responsive">
+					<table class="table-hover" id="users">
+						<tbody>
 <?php
 ################ BUILD TABLE ###################################
 
@@ -236,29 +236,69 @@ if(!$result = $db->query($sql)) {
 while($row = $result->fetch_assoc()) {
 	if($row['snapchat_username'] == $_SESSION['snapchat_username']) continue;
 	?>
-			<tr>
-				<td><a href="//facebook.com/<?=$row['facebook_id']?>" target="_blank"><img src="//graph.facebook.com/<?=$row['facebook_id']?>/picture" /></a></td>
-				<td><?=$row['realname']?></td>
-				<td><?=$row['snapchat_username']?></td>
-				<td class="switcher-box">
-					<div class="btn-group">
-						<input type="checkbox" class="switcher" data-user="<?=$row['snapchat_username']?>" <?=(isset($snapFriends[$row['snapchat_username']]))?"checked":""?>>
-					</div>
-				</td>
-			</tr>
+							<tr>
+								<td><a href="//facebook.com/<?=$row['facebook_id']?>" target="_blank"><img src="//graph.facebook.com/<?=$row['facebook_id']?>/picture" /></a></td>
+								<td><?=$row['realname']?></td>
+								<td><?=$row['snapchat_username']?></td>
+								<td class="switcher-box">
+									<div class="btn-group">
+										<input type="checkbox" class="switcher" data-user="<?=$row['snapchat_username']?>" <?=(isset($snapFriends[$row['snapchat_username']]))?"checked":""?>>
+									</div>
+								</td>
+							</tr>
 	<?php
 }
 ?>
-		</tbody>
-	</table>
-</div>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="tab-pane fade" id="rename">
+				<h4>Batch Rename</h4>
+				<p>
+					This tool will rename all of your snapchat people, and setting their names to their Facebook name, optionally with a prefix like an anchor.
+					Sometimes it fails; if it does, you'll see some lines end with "false". I'm not sure why this happens sometimes, but if you try again,
+					it should work. I'm sorry it's not very pretty. &#x2693;
 
+					<form class="form-horizontal" action="rename.php" method="POST" role="form">
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="prefix">Prefix</label>
+							<div class="col-sm-4">
+								<input type="text" class="form-control" name="prefix" id="prefix" placeholder="Prefix (optional)">
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+								<button type="submit" class="btn btn-primary">Rename</button>
+							</div>
+						</div>
+					</form>
+				</p>
+			</div>
+			<div class="tab-pane fade" id="settings">
+				<p>
+					You'll automatically be added to the list of users when you come to this page, and other shippers can add you to
+					their friends. But maybe you're a creeper and you don't want anyone to add you. That's cool, bro, just turn yourself
+					hidden.
+				</p>
+				<div class="form-group">
+					<label class="col-sm-2 control-label" for="prefix">Visible</label>
+					<div class="col-sm-4">
+						<div class="btn-group">
+							<input type="checkbox" class="hider" <?=(!$hidden)?"checked":""?>>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
 			<?php endif ?>
+
+
 	</div>
 
     <div id="footer">
     	<div class="container">
-    		<p class="text-muted">This was written by <a href="https://philihp.com">Philihp Busby</a> to make your life easier.</p>
+    		<p class="text-muted">Created by <a href="https://philihp.com">Philihp Busby</a> to make your life easier. Design by <a href="http://getbootstrap.com/">Twitter Bootstrap</a>.</p>
     	</div>
     </div>
 	<script src="js/jquery.min.js"></script>
@@ -306,6 +346,14 @@ while($row = $result->fetch_assoc()) {
 			}
 		});
 
+		<?php if(isset($snapchat)): ?>
+		$('#tabs a').click(function (e) {
+			e.preventDefault()
+			$(this).tab('show')
+		})
+
+
+		<?php endif ?>
 /*
 		$(function() {
 			var tour = new Tour({
---
+++ rename.php
@@ -0,0 +1,56 @@
+<?php
+error_reporting(~0);
+ini_set('display_errors', 1);
+
+require_once('php-snapchat/src/snapchat.php');
+
+session_start();
+
+$db = new mysqli('localhost','boatsnap','boatsnap1','boatsnap');
+if($db->connect_errno > 0) {
+	die('Could not connect to MySQL. Please contact the Holy Ship IT Department.');
+}
+$sql = <<<SQL
+	SELECT snapchat_username, realname
+	  FROM user
+	  WHERE snapchat_username IS NOT NULL
+	    AND hidden = 0
+SQL;
+if(!$result = $db->query($sql)) {
+	die('Unable to query database. Please contact Holy Ship IT Department.');
+}
+while($row = $result->fetch_assoc()) {
+	$lookup[$row['snapchat_username']] = $row['realname'];
+}
+
+/* load snapchat out of session */
+if(isset($_SESSION['snapchat'])) {
+	$snapchat = $_SESSION['snapchat'];
+}
+else {
+	die('Not logged into Snapchat');
+}
+
+$friends = $snapchat->getFriends();
+$prefix = isset($_POST['prefix'])?$_POST['prefix']:"";
+
+foreach($snapchat->getFriends() as $friend) {
+	if(!isset($lookup[$friend->name])) {
+		echo "Skipping \"{$friend->name}\"; don't know user's real name.<br />\n";
+		continue;
+	}
+	$display = $prefix.($lookup[$friend->name]);
+	if($friend->display == $display) {
+		echo "Skipping \"{$friend->name}\"; name is already set to $display.<br />\n";
+		continue;
+	}
+
+	$o = new stdClass();
+	$o->snapchat_username = $friend->name;
+	$o->newDisplayName = $display;
+	$o->returned = $snapchat->setDisplayName($friend->name, $display)?"OK":"Failed!";
+	echo "Setting \"$friend->name\" to \"$display\"... $o->returned<br />\n";
+	flush();
+}
+
+?>
